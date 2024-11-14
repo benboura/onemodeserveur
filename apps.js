380dfaf73647176db62414c12ded69def822d3d4
benboura <bourabenraphael@gmail.com>
2024-11-14 15:43:49 +0300

    14/11

--- apps.js
+++ apps.js
@@ -1,18 +1,45 @@
 // import du framework ExpressJs
-import express from "express";
+//import express from "express";
 
-const express = require ("express")
-
-// on crée l'application ExpressJs
-const app = express();
+const express = require ("express");
 
 
-app.get("/bonjour", (req, res )=> {
-    res.end("Salam, 🫱🏼‍🫲🏾");
+const apps = express();
+
+
+//'app.use' pour géneré les requêtes et envoyé le message mentionner vers le client 
+/*apps.use((req, res) => {
+    res.json({message : "votre message a bien été reçue"});
+})*/
+
+apps.get("/", (req, res )=> {
+    res.end("Salam, ");
+});
+// route pour l'accueil
+apps.get("/accueil", (req, res )=> {
+    res.end("c'est ici l'accueil !!");
+});
+
+// route pour apropos
+apps.get("/apropos", (req, res )=> {
+    res.end("A bons port sur apropos !!");
+});
+
+// route pour services 
+apps.get("/services", (req, res )=> {
+    res.end(" welcom to service  !!");
+});
+
+// route pour contacts 
+apps.get("/contacts", (req, res )=> {
+    res.end("Directement par mail ou par tel, on repond spontanement !!!");
 });
 
 
+// partager l'objet 'apps' avec d'autre fichiers
+module.exports = apps;
 
-app.listen(3001, ()=> {
+/* app.listen(3001, ()=> {
     console.log("serveur écoute le port 3001");
 });
+*/
--- serveur.js
+++ serveur.js
@@ -3,19 +3,31 @@
 // ici le package importé est http
 const http = require('http')
 
-// const app = require("./app");
+const apps = require('./apps');
 
-// app.set("port",3000);
+const numPort = 3002;
+
+// Crée un nouvel objet Date avec l'heure actuelle
+const currentDate = new Date; 
+// Récupère l'heure au format local
+const Time = currentDate.toLocaleTimeString(); 
 
 
-// const server1 = createServer(app);
+apps.set("port",numPort);
+
+const server = http.createServer(apps);
+
+server.listen(3002, () => {
+    console.log("Il est: ",Time );
+    console.log("le serveur est activé au port: ", numPort);
+});
 
 // declaration du serveur en utilisant http
-const server = http.createServer((req, res)=> {
+// const server = http.createServer((req, res)=> {
     // pour envoyer le message de réponse, 'res' suivi de 'end' 
-    res.end("Bonjour, je suis le serveur.");
-})
+    //res.end("Bonjour, je suis le serveur.");
+//})
 // le serveur est disponible sur le port 3000
-server.listen(3000);
+
 
 // localhost signifie le serveur en local 
